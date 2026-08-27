ee4e2ac1b08bf66ba2f42a21b2bfdcf45e68b2b0
Alireza Alavi <alireza.alavi237@gmail.com>
2026-08-27 18:43:07 +0200

    refactor memory.py to implement IBuffer interface and improve sample method

--- src/agent/memory.py
+++ src/agent/memory.py
@@ -1,3 +1,5 @@
+import random
+from abc import ABC, abstractmethod
 from collections import deque
 from dataclasses import dataclass
 
@@ -16,16 +18,55 @@ class Transition:
 	done: bool
 
 
-class ReplayBuffer:
+@dataclass(frozen=True)
+class Batch:
+	states: npt.NDArray[np.float32]
+	actions: npt.NDArray[np.float32]
+	rewards: npt.NDArray[np.float32]
+	next_states: npt.NDArray[np.float32]
+	dones: npt.NDArray[np.float32]
+
+
+class IBuffer(ABC):
+	@abstractmethod
+	def add(self, transition: Transition) -> None:
+		"""Stores a transition in the buffer."""
+		pass
+
+	@abstractmethod
+	def sample(self, batch_size: int) -> Batch:
+		"""Samples a batch and returns arrays formatted for training."""
+		pass
+
+	@abstractmethod
+	def __len__(self) -> int:
+		"""Returns the number of transitions currently stored."""
+		pass
+
+	@abstractmethod
+	def clear(self) -> None:
+		"""Clears all the transitions currently stored in the buffer."""
+		pass
+
+
+class UniformReplayBuffer(IBuffer):
 	def __init__(self, capacity: int) -> None:
 		self._buffer: deque[Transition] = deque(maxlen=capacity)
 
 	def add(self, transition: Transition) -> None:
 		self._buffer.append(transition)
 
-	def sample(self, batch_size: int) -> npt.NDArray:
-		return np.random.choice(
-			np.asarray(self._buffer), size=batch_size, replace=False
+	def sample(self, batch_size: int) -> Batch:
+		batch: list[Transition] = random.sample(self._buffer, k=batch_size)
+
+		return Batch(
+			states=np.array([t.state.nparray() for t in batch], dtype=np.float32),
+			actions=np.array([t.action for t in batch], dtype=np.float32),
+			rewards=np.array([t.reward for t in batch], dtype=np.float32),
+			next_states=np.array(
+				[t.next_state.nparray() for t in batch], dtype=np.float32
+			),
+			dones=np.array([t.done for t in batch], dtype=np.float32),
 		)
 
 	def __len__(self) -> int:
