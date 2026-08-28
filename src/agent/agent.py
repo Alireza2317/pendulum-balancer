84d224cca2c1e2e4f04d26f1f653fa626bcffc99
Alireza Alavi <alireza.alavi237@gmail.com>
2026-08-28 03:18:44 +0200

    implement DDPGAgent with training and target network update functionality

---
+++ src/agent/agent.py
@@ -0,0 +1,102 @@
+from dataclasses import dataclass
+
+import tensorflow as tf
+import tensorflow.keras as tfk
+
+from physics.state import EnvState
+from src.agent.models import Actor, Critic
+
+
+@dataclass(frozen=True)
+class DDPGConfig:
+	gamma: float = 0.99
+	tau: float = 0.005
+
+
+class DDPGAgent:
+	def __init__(self, cfg: DDPGConfig = DDPGConfig()) -> None:
+		self.cfg = cfg
+
+		self.actor = Actor()
+		self.critic = Critic()
+		self.target_actor = Actor()
+		self.target_critic = Critic()
+
+		self.target_actor.set_weights(self.actor.get_weights())
+		self.target_critic.set_weights(self.critic.get_weights())
+
+		self.actor_optimizer = tfk.optimizers.Adam(learning_rate=0.001)
+		self.critic_optimizer = tfk.optimizers.Adam(learning_rate=0.002)
+
+	@tf.function
+	def update_target_networks(self) -> None:
+		"""Applies Polyak averaging (theta' = tau * theta + (1-tau) * theta')."""
+
+		for target, main in zip(self.target_actor.variables, self.actor.variables):
+			target.assign(target * (1 - self.cfg.tau) + main * self.cfg.tau)
+
+		for target, main in zip(self.target_critic.variables, self.critic.variables):
+			target.assign(target * (1 - self.cfg.tau) + main * self.cfg.tau)
+
+	def get_action(self, state: tf.Tensor, noise_std: float = 0.1) -> float:
+		"""Runs inference and adds exploration noise during training."""
+		# Expand state's dimension from (n, ) to (1, n)
+		state_tensor: tf.Tensor = tf.expand_dims(state, axis=0)
+
+		action: tf.Tensor = self.actor(state_tensor)[0, 0]
+
+		if noise_std > 0:
+			noise = tf.random.normal(shape=(), mean=0, stddev=noise_std)
+			action += noise
+
+		return float(tf.clip_by_value(action, -1, 1))
+
+	@tf.function
+	def train_step(
+		self,
+		states: tf.Tensor,
+		actions: tf.Tboolensor,
+		rewards: tf.Tensor,
+		next_states: tf.Tensor,
+		dones: tf.Tensor,
+	) -> tuple[tf.Tensor, tf.Tensor]:
+		"""
+		Calculates loss and applies gradients for one sampled batch.
+		Returns actor_loss and critic_loss
+		"""
+		# Update Critic
+		with tf.GradientTape() as tape:
+			# The critic should have outputs close to target values y
+			# y = r + gamma * Q(s', mu'(s'))
+			# Calculate the target value
+			next_actions: tf.Tensor = self.target_actor(next_states)
+			# Future rewards
+			target_q_values: tf.Tensor = self.target_critic(next_states, next_actions)
+			y: tf.Tensor = rewards + self.cfg.gamma * target_q_values
+
+			# MSE loss
+			predicted_q_values: tf.Tensor = self.critic(states, actions)
+			critic_loss: tf.Tensor = tf.reduce_mean(tf.square(y - predicted_q_values))
+
+		critic_grads = tape.gradient(critic_loss, self.critic.trainable_variables)
+		self.critic_optimizer.apply_gradients(
+			zip(critic_grads, self.critic.trainable_variables)
+		)
+
+		# Update Actor
+		with tf.GradientTape() as tape:
+			# The actor should maximize the critic's reward predictions Q(s, mu(s))
+			actor_actions: tf.Tensor = self.actor(states)
+
+			# Since gradients try to MINIMIZE, the actor should minimize -Q(s, mu(s))
+			actor_loss: tf.Tensor = -tf.reduce_mean(self.critic(states, actor_actions))
+
+		actor_grads = tape.gradient(actor_loss, self.actor.trainable_variables)
+		self.actor_optimizer.apply_gradients(
+			zip(actor_grads, self.actor.trainable_variables)
+		)
+
+		# Update the target networks, very slightly and softly
+		self.update_target_networks()
+
+		return actor_loss, critic_loss
