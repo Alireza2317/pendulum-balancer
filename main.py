8b83096cdaf308d6e671db26c36bee3b820c4544
Alireza Alavi <alireza.alavi237@gmail.com>
2026-08-30 05:08:40 +0200

    implement main entry point to do training

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-import time
+import os
+import random
 
+import numpy as np
+import tensorflow as tf
+
+from src.agent.agent import DDPGAgent
+from src.agent.memory import UniformReplayBuffer
+from src.checkpointer.checkpointer import ModelCheckpointer
 from src.physics.env import DoublePendulumEnv
+from src.trainer.trainer import DDPGTrainer
 
-env = DoublePendulumEnv()
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+random.seed(23)
+np.random.seed(23)
+tf.random.set_seed(23)
 
-for i in range(10000):
-	env.step(0)
-	time.sleep(1 / 240)
 
-env.close()
+def main():
+	env = DoublePendulumEnv(render=True)
+	buffer = UniformReplayBuffer(10_000)
+	agent = DDPGAgent()
+	trainer = DDPGTrainer(env, agent, buffer, batch_size=32)
+	checkpointer = ModelCheckpointer(agent)
 
+	checkpointer.load_latest()
+
+	start_episode: int = int(checkpointer.episode_counter) + 1
+	MAX_EPISODES: int = 1_000
+
+	for episode in range(start_episode, start_episode + MAX_EPISODES):
+		print(f"Running episode {episode:4}...")
+
+		episode_reward = trainer.run_episode()
+
+		if episode % 20 == 0:
+			checkpointer.episode_counter.assign(episode)
+			checkpointer.log_scalar(
+				"Episode Total Reward", episode_reward, step=episode
+			)
+			checkpointer.save(episode)
+
+
+if __name__ == "__main__":
+	main()
