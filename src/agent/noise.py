661fe572863084870166d387df4b6f0fd22f7fda
Alireza Alavi <alireza.alavi237@gmail.com>
2026-09-02 07:51:26 +0200

    add a sigma setter to OUNoise

--- src/agent/noise.py
+++ src/agent/noise.py
@@ -8,19 +8,26 @@ class OUNoise:
 
 	def __init__(self, config: Config) -> None:
 		self.cfg = config
-		self.sigma: float = self.cfg.ounoise_sigma
+		self._sigma: float = self.cfg.ounoise_sigma
 		self.reset()
 
 	def reset(self) -> None:
 		"""Reset internal state to mean (to be called at the start of every episode)."""
 		self.state = np.ones(self.cfg.action_dim) * self.cfg.ounoise_mu
 
+	@property
+	def sigma(self) -> float:
+		return self._sigma
+
+	def set_sigma(self, value: float) -> None:
+		self._sigma = value
+
 	def decay(self) -> None:
 		"""
 		Anneals exploration magnitude toward ounoise_sigma_min. To be Called once
 		per episode."""
-		self.sigma = max(
-			self.cfg.ounoise_sigma_min, self.sigma * self.cfg.ounoise_decay
+		self._sigma = max(
+			self.cfg.ounoise_sigma_min, self._sigma * self.cfg.ounoise_decay
 		)
 
 	def sample(self) -> float:
